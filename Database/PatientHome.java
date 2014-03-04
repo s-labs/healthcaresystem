4fec8324fbf293f14b340495eb51e36e0d04464d
s-labs <kranthi.p@s-labs.in>
2014-03-04 09:12:22 +0530

    added auto generated patient java file

---
+++ Database/PatientHome.java
@@ -0,0 +1,122 @@
+// default package
+// Generated Mar 4, 2014 9:07:27 AM by Hibernate Tools 3.6.0
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class Patient.
+ * @see .Patient
+ * @author Hibernate Tools
+ */
+public class PatientHome {
+
+	private static final Log log = LogFactory.getLog(PatientHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(Patient transientInstance) {
+		log.debug("persisting Patient instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(Patient instance) {
+		log.debug("attaching dirty Patient instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(Patient instance) {
+		log.debug("attaching clean Patient instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(Patient persistentInstance) {
+		log.debug("deleting Patient instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public Patient merge(Patient detachedInstance) {
+		log.debug("merging Patient instance");
+		try {
+			Patient result = (Patient) sessionFactory.getCurrentSession()
+					.merge(detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public Patient findById(java.lang.Integer id) {
+		log.debug("getting Patient instance with id: " + id);
+		try {
+			Patient instance = (Patient) sessionFactory.getCurrentSession()
+					.get("Patient", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(Patient instance) {
+		log.debug("finding Patient instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("Patient").add(Example.create(instance))
+					.list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
---
+++ Database/StateHome.java
@@ -0,0 +1,122 @@
+// default package
+// Generated Mar 4, 2014 9:07:27 AM by Hibernate Tools 3.6.0
+
+import java.util.List;
+import javax.naming.InitialContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.LockMode;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Example;
+
+/**
+ * Home object for domain model class State.
+ * @see .State
+ * @author Hibernate Tools
+ */
+public class StateHome {
+
+	private static final Log log = LogFactory.getLog(StateHome.class);
+
+	private final SessionFactory sessionFactory = getSessionFactory();
+
+	protected SessionFactory getSessionFactory() {
+		try {
+			return (SessionFactory) new InitialContext()
+					.lookup("SessionFactory");
+		} catch (Exception e) {
+			log.error("Could not locate SessionFactory in JNDI", e);
+			throw new IllegalStateException(
+					"Could not locate SessionFactory in JNDI");
+		}
+	}
+
+	public void persist(State transientInstance) {
+		log.debug("persisting State instance");
+		try {
+			sessionFactory.getCurrentSession().persist(transientInstance);
+			log.debug("persist successful");
+		} catch (RuntimeException re) {
+			log.error("persist failed", re);
+			throw re;
+		}
+	}
+
+	public void attachDirty(State instance) {
+		log.debug("attaching dirty State instance");
+		try {
+			sessionFactory.getCurrentSession().saveOrUpdate(instance);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void attachClean(State instance) {
+		log.debug("attaching clean State instance");
+		try {
+			sessionFactory.getCurrentSession().lock(instance, LockMode.NONE);
+			log.debug("attach successful");
+		} catch (RuntimeException re) {
+			log.error("attach failed", re);
+			throw re;
+		}
+	}
+
+	public void delete(State persistentInstance) {
+		log.debug("deleting State instance");
+		try {
+			sessionFactory.getCurrentSession().delete(persistentInstance);
+			log.debug("delete successful");
+		} catch (RuntimeException re) {
+			log.error("delete failed", re);
+			throw re;
+		}
+	}
+
+	public State merge(State detachedInstance) {
+		log.debug("merging State instance");
+		try {
+			State result = (State) sessionFactory.getCurrentSession().merge(
+					detachedInstance);
+			log.debug("merge successful");
+			return result;
+		} catch (RuntimeException re) {
+			log.error("merge failed", re);
+			throw re;
+		}
+	}
+
+	public State findById(java.lang.Integer id) {
+		log.debug("getting State instance with id: " + id);
+		try {
+			State instance = (State) sessionFactory.getCurrentSession().get(
+					"State", id);
+			if (instance == null) {
+				log.debug("get successful, no instance found");
+			} else {
+				log.debug("get successful, instance found");
+			}
+			return instance;
+		} catch (RuntimeException re) {
+			log.error("get failed", re);
+			throw re;
+		}
+	}
+
+	public List findByExample(State instance) {
+		log.debug("finding State instance by example");
+		try {
+			List results = sessionFactory.getCurrentSession()
+					.createCriteria("State").add(Example.create(instance))
+					.list();
+			log.debug("find by example successful, result size: "
+					+ results.size());
+			return results;
+		} catch (RuntimeException re) {
+			log.error("find by example failed", re);
+			throw re;
+		}
+	}
+}
